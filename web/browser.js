386bed5e28d823a0bcea76e09d6fe1c54a0378ba
Bence Sgánetz <voidxb@gmail.com>
2021-04-12 17:58:28 +0200

    Browser sorting

---
+++ web/browser.js
@@ -0,0 +1,132 @@
+hidden = [
+  "$recycle.bin",
+  "config.msi",
+  "desktop.ini",
+  "system volume information",
+]
+
+function fixBrowser() {
+  var table = get("files");
+  for (var i = 0, row; row = table.rows[i]; i++) {
+    var entry = row.cells[0].childNodes[0].innerHTML;
+    if (hidden.indexOf(entry.toLowerCase()) >= 0) {
+      table.deleteRow(i);
+      --i;
+      continue;
+    }
+    for (var j = 0, col; col = row.cells[j]; j++) {
+      var node = col.childNodes[0];
+      if (node.innerHTML == null)
+        continue;
+      var l = node.innerHTML.length;
+      if (l > 0) {
+        if (node.innerHTML === "Ogg Vorbis") {
+          node.innerHTML = "Xiph";
+          continue;
+        } else if (node.innerHTML.endsWith("_auto_file")) {
+          var fmt = node.innerHTML.substring(0, node.innerHTML.length - 10);
+          if (fmt === "lrc")
+            node.innerHTML = "LyRiCs";
+          else if (fmt === "srt")
+            node.innerHTML = "SubRip";
+          else
+            node.innerHTML = fmt.toUpperCase() + " file";
+          continue;
+        }
+        var size = parseInt(node.innerHTML);
+        if (!isNaN(size) && node.innerHTML.indexOf(".") == -1) {
+          if (size < 1024)
+            node.innerHTML = size + " kB";
+          else if (size > 1048576)
+            node.innerHTML = Math.round(size / 1048576 * 10) / 10 + " GB";
+          else
+            node.innerHTML = Math.round(size / 1024 * 10) / 10 + " MB";
+        }
+      }
+    }  
+  }
+}
+
+function updateSearch() {
+  var table = get("files");
+  var string = get("search").value.toLowerCase();
+  for (var i = 1, row; row = table.rows[i]; i++) {
+    var entry = row.cells[0].childNodes[0].innerHTML.toLowerCase();
+    var next = string.length == 0 || entry.includes(string) ? "table-row" : "none";
+    if (row.style.display != next)
+      row.style.display = next;
+  }
+}
+
+function resetSearch() {
+  get("search").value = "";
+  updateSearch();
+}
+
+function swap(a, b) {
+  var source = [];
+  for (var i = 0; i < b.childNodes.length; ++i)
+    source[i] = b.childNodes[i].innerHTML;
+  for (var i = 0; i < b.childNodes.length; ++i)
+    b.childNodes[i].innerHTML = a.childNodes[i].innerHTML;
+  for (var i = 0; i < a.childNodes.length; ++i)
+    a.childNodes[i].innerHTML = source[i];
+}
+
+function partition(rows, low, high, col) {
+  pivot = high;
+  i = low;
+  for (var j = low; j <= high; ++j) {
+    var a = rows[j].cells[col].childNodes[0].innerHTML;
+    a = a ? a.toLowerCase() : "";
+    var b = rows[pivot].cells[col].childNodes[0].innerHTML;
+    b = b ? b.toLowerCase() : "";
+    if (a < b) {
+      swap(rows[i], rows[j]);
+      ++i;
+    }
+  }
+  swap(rows[i], rows[high]);
+  return i
+}
+
+function quicksort(rows, low, high, col) {
+  if (low < high) {
+    p = partition(rows, low, high, col);
+    quicksort(rows, low, p - 1, col);
+    quicksort(rows, p + 1, high, col);
+  }
+}
+
+function reverse(rows, from, to) {
+  var end = (to - from) / 2;
+  for (var i = 0; i < end; ++i)
+    swap(rows[from + i], rows[to - i]);
+}
+
+lastName = true;
+lastAsc = true;
+btn1 = "btn btn-primary";
+btn0 = "btn btn-secondary";
+function sort(name, asc) {
+  get("nameup").className = name && asc ? btn1 : btn0;
+  get("namedown").className = name && !asc ? btn1 : btn0;
+  get("dateup").className = !name && asc ? btn1 : btn0;
+  get("datedown").className = !name && !asc ? btn1 : btn0;
+  var rows = get("files").rows;
+  var folders;
+  for (folders = 1; folders < rows.length; ++folders)
+    if (rows[folders].cells[0].className != "dirname")
+      break;
+  if (lastName != name) {
+    var col = name ? 0 : 3;
+    quicksort(rows, 1, folders - 1, col);
+    quicksort(rows, folders, rows.length - 1, col);
+  }
+  if ((lastName == name && lastAsc != asc) || (lastName != name && !asc)) {
+    reverse(rows, 1, folders - 1);
+    reverse(rows, folders, rows.length - 1);
+  }
+  lastName = name;
+  lastAsc = asc;
+}
