16210f76d94bd81ae9a9668a96cdaf569d677540
Bence Sgánetz <voidxb@gmail.com>
2021-04-19 00:08:37 +0200

    Remove lots of JS because JS sucks

--- web/browser.js
+++ web/browser.js
@@ -1,59 +1,45 @@
-hidden = [
-  "$recycle.bin",
-  "config.msi",
-  "desktop.ini",
-  "system volume information",
-]
-
+hidden = [ "$recycle.bin", "config.msi", "desktop.ini",
+  "system volume information" ];
+sizes = [ "kB", "MB", "GB", "TB", "PB", "EB", "YB" ];
 filterTypes = [ "all", "vid", "aud" ];
-
+videoFormats = [ "3gp", "3g2", "avi", "f4v", "flv", "m2ts", "mk3d", "mkv",
+  "mp4", "mpeg", "mpg", "mov", "mp4", "mxf", "ogv", "ps", "ts", "webm", "wmv" ];
+audioFormats = [ "aac", "aiff", "alac", "cda", "flac", "m4a", "mp3", "ogg",
+  "opus", "wav", "wma", "weba" ];
 btn1 = "btn btn-primary";
 btn0 = "btn btn-secondary";
+selectedRow = 0; // for navigation by keys
+lastName = true; // for sorting
+lastAsc = true;
 
 function fixBrowser() {
   var table = get("files");
   for (var i = 0, row; row = table.rows[i]; ++i) {
     var entry = row.cells[0].childNodes[0].innerHTML;
     if (hidden.indexOf(entry.toLowerCase()) >= 0) {
-      table.deleteRow(i);
-      --i;
+      table.deleteRow(i--);
       continue;
     }
-    for (var j = 0, col; col = row.cells[j]; j++) {
-      var node = col.childNodes[0];
-      if (node.innerHTML == null)
-        continue;
-      var l = node.innerHTML.length;
-      if (l > 0) {
-        if (node.innerHTML === "Ogg Vorbis") {
-          node.innerHTML = "Xiph";
-          continue;
-        } else if (node.innerHTML.endsWith("_auto_file")) {
-          var fmt = node.innerHTML.substr(0, node.innerHTML.length - 10);
-          if (fmt === "lrc")
-            node.innerHTML = "LyRiCs";
-          else if (fmt === "srt")
-            node.innerHTML = "SubRip";
-          else
-            node.innerHTML = fmt.toUpperCase() + " file";
-          continue;
-        }
-        var size = parseInt(node.innerHTML);
-        if (!isNaN(size) && node.innerHTML.indexOf(".") == -1) {
-          if (size < 1024)
-            node.innerHTML = size + " kB";
-          else if (size > 1048576)
-            node.innerHTML = Math.round(size / 1048576 * 10) / 10 + " GB";
-          else
-            node.innerHTML = Math.round(size / 1024 * 10) / 10 + " MB";
-        }
-      }
-    }  
+    var type = row.cells[1].childNodes[0];
+    if (type.innerHTML === "Ogg Vorbis") {
+      type.innerHTML = "Xiph";
+    } else if (type.innerHTML != null && type.innerHTML.endsWith("_auto_file")) {
+      var fmt = type.innerHTML.substr(0, type.innerHTML.length - 10);
+      if (fmt === "lrc")
+        type.innerHTML = "LyRiCs";
+      else if (fmt === "srt")
+        type.innerHTML = "SubRip";
+      else
+        type.innerHTML = fmt.toUpperCase() + " file";
+    }
+    var node = row.cells[2].childNodes[0], size = parseInt(node.innerHTML);
+    if (!isNaN(size) && node.innerHTML.indexOf(".") == -1) {
+      var log = Math.floor(Math.log(size) / Math.log(1024));
+      node.innerHTML = Math.round(size / Math.pow(1024, log) * 10) / 10 + ' ' + sizes[log];
+    }
   }
   setupKeys();
 }
-
-selectedRow = 0;
 
 function setupKeys() {
   addEventListener('keydown', function(event) {
@@ -118,11 +104,6 @@ function updateSearch() {
   }
 }
 
-function resetSearch() {
-  get("search").value = "";
-  updateSearch();
-}
-
 function swap(a, b) {
   var source = [];
   for (var i = 0; i < b.childNodes.length; ++i)
@@ -134,17 +115,14 @@ function swap(a, b) {
 }
 
 function partition(rows, low, high, col) {
-  pivot = high;
-  i = low;
+  var pivot = high, i = low;
   for (var j = low; j <= high; ++j) {
     var a = rows[j].cells[col].childNodes[0].innerHTML;
     a = a ? a.toLowerCase() : "";
     var b = rows[pivot].cells[col].childNodes[0].innerHTML;
     b = b ? b.toLowerCase() : "";
-    if (a < b) {
-      swap(rows[i], rows[j]);
-      ++i;
-    }
+    if (a < b)
+      swap(rows[i++], rows[j]);
   }
   swap(rows[i], rows[high]);
   return i
@@ -152,28 +130,23 @@ function partition(rows, low, high, col) {
 
 function quicksort(rows, low, high, col) {
   if (low < high) {
-    p = partition(rows, low, high, col);
+    var p = partition(rows, low, high, col);
     quicksort(rows, low, p - 1, col);
     quicksort(rows, p + 1, high, col);
   }
 }
 
 function reverse(rows, from, to) {
-  var end = (to - from) / 2;
-  for (var i = 0; i < end; ++i)
+  for (var i = 0, end = (to - from) / 2; i < end; ++i)
     swap(rows[from + i], rows[to - i]);
 }
-
-lastName = true;
-lastAsc = true;
 
 function sort(name, asc) {
   get("nameup").className = name && asc ? btn1 : btn0;
   get("namedown").className = name && !asc ? btn1 : btn0;
   get("dateup").className = !name && asc ? btn1 : btn0;
   get("datedown").className = !name && !asc ? btn1 : btn0;
-  var rows = get("files").rows;
-  var folders;
+  var rows = get("files").rows, folders;
   for (folders = 1; folders < rows.length; ++folders)
     if (rows[folders].cells[0].className != "dirname")
       break;
@@ -190,11 +163,6 @@ function sort(name, asc) {
   lastAsc = asc;
 }
 
-videoFormats = [ "3gp", "3g2", "avi", "f4v", "flv", "m2ts", "mk3d", "mkv",
-  "mp4", "mpeg", "mpg", "mov", "mp4", "mxf", "ogv", "ps", "ts", "webm", "wmv" ];
-audioFormats = [ "aac", "aiff", "alac", "cda", "flac", "m4a", "mp3", "ogg",
-  "opus", "wav", "wma", "weba" ];
-
 function setType(type) {
   get("search").value = "";
   for (var i = 0; i < filterTypes.length; ++i)
@@ -202,14 +170,12 @@ function setType(type) {
   var table = get("files");
   if (type == "all") {
     for (var i = 1, row; row = table.rows[i]; ++i)
-      if (row.style.display != "table-row")
-        row.style.display = "table-row";
+      row.style.display = "table-row";
   } else {
     var target = type == "vid" ? videoFormats : audioFormats;
     for (var i = 1, row; row = table.rows[i]; ++i) {
       var next = target.includes(row.className) ? "table-row" : "none";
-      if (row.style.display != next)
-        row.style.display = next;
+      row.style.display = next;
     }
   }
 }
