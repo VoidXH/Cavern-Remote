056f8dbb394044c4915c2f21b5c1edced0ca7967
Bence Sgánetz <voidxb@gmail.com>
2021-04-29 22:39:33 +0200

    Double relay command

---
+++ src/CavernRemoteCGI/Commands/DoubleRelayCommand.cs
@@ -0,0 +1,60 @@
+﻿using CavernRemoteCGI.Tools;
+using System;
+
+namespace CavernRemoteCGI.Commands {
+    public class DoubleRelayCommand : Command {
+        public override string Help => "drelay=<port>;<power>;<a>;<b>;<on>;<astate>;<bstate>: Handle double relays.";
+
+        static bool ParseBool(string text, out bool result) {
+            if (bool.TryParse(text, out result))
+                return true;
+            if (int.TryParse(text, out int num)) {
+                result = num != 0;
+                return true;
+            }
+            return false;
+        }
+
+        public override void Run(string param) {
+            string[] vars = param.Split(';');
+            if (vars.Length != 7) {
+                Console.WriteLine("Incorrect number of parameters.");
+                return;
+            }
+            if (!int.TryParse(vars[1], out int power) ||
+                !int.TryParse(vars[2], out int a) ||
+                !int.TryParse(vars[3], out int b)) {
+                Console.WriteLine("A pin was not a number.");
+                return;
+            }
+            if (!ParseBool(vars[4], out bool on) ||
+                !ParseBool(vars[5], out bool astate) ||
+                !ParseBool(vars[6], out bool bstate)) {
+                Console.WriteLine("A state was not boolean.");
+                return;
+            }
+
+            Firmata board;
+            try {
+                board = new Firmata(vars[0]);
+            } catch (UnauthorizedAccessException _) {
+                Console.WriteLine("Access is denied to the port.");
+                return;
+            } catch (ArgumentException _) {
+                Console.WriteLine("The port name does not begin with \"COM\", or the file type of the port is not supported.");
+                return;
+            } catch (InvalidOperationException _) {
+                Console.WriteLine("The specified port is used by another application.");
+                return;
+            } catch {
+                Console.WriteLine("The port couldn't be opened.");
+                return;
+            }
+
+            board.SetPin(a, !astate);
+            board.SetPin(b, !bstate);
+            board.SetPin(power, on);
+            board.Dispose();
+        }
+    }
+}
---
+++ src/CavernRemoteCGI/Tools/Firmata.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace CavernRemoteCGI.Tools {
+    public enum PinMode {
+        Input = 0,
+        Output = 1,
+        Analog = 2,
+        PWM = 3,
+        Servo = 4,
+        I2C = 6,
+        OneWire = 7,
+        Stepper = 8,
+        Encoder = 9,
+        Serial = 10,
+        Pullup = 11
+    }
+
+    public class Firmata : IDisposable {
+        const int setPinMode = 0xF4;
+        const int digitalWrite = 0xF5;
+
+        readonly SerialPort port;
+
+        public Firmata(string portName, int baudRate = 57600) {
+            port = new SerialPort(portName, baudRate) {
+                DataBits = 8,
+                Parity = Parity.None,
+                StopBits = StopBits.One
+            };
+            port.Open();
+        }
+
+        public void SetPin(int pin, PinMode mode) {
+            byte[] message = new byte[] { setPinMode, (byte)pin, (byte)mode };
+            port.Write(message, 0, message.Length);
+        }
+
+        public void SetPin(int pin, bool state) {
+            byte[] message = new byte[] { digitalWrite, (byte)pin, state ? (byte)1 : (byte)0 };
+            port.Write(message, 0, message.Length);
+        }
+
+        public void Dispose() {
+            if (port != null)
+                port.Close();
+        }
+    }
+}
