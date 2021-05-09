0f253fcf6b2817bcaaef365cb7cf8556bf150374
Bence Sgánetz <voidxb@gmail.com>
2021-05-09 15:58:06 +0200

    Shuffle large folders and do it effectively

--- src/CavernRemoteCGI/Commands/ShuffleFolderCommand.cs
+++ src/CavernRemoteCGI/Commands/ShuffleFolderCommand.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
+using System.Threading;
 
 namespace CavernRemoteCGI.Commands {
     public class ShuffleFolderCommand : Command {
@@ -14,14 +14,27 @@ namespace CavernRemoteCGI.Commands {
             }
             string[] files = Directory.GetFiles(path);
             Random rnd = new Random();
-            files = files.OrderBy(x => rnd.Next()).ToArray();
+            rnd.Shuffle(files);
             Process[] proc = GetPlayer();
             if (proc.Length != 0) {
                 ProcessStartInfo info = proc[0].StartInfo;
-                info.Arguments = "\"" + string.Join("\" \"", files) + '"';
                 info.FileName = proc[0].GetMainModuleFileName();
+                info.Arguments = "\"" + string.Join("\" \"", files) + '"';
+                bool safeWay = info.FileName.Length + info.Arguments.Length > 2048;
+
+                string playlistName = Path.Combine(path, path.GetHashCode().ToString() + ".m3u");
+                if (safeWay) {
+                    File.WriteAllLines(playlistName, files);
+                    info.Arguments = $"\"{playlistName}\"";
+                }
+
                 proc[0].StartInfo = info;
                 proc[0].Start();
+
+                if (safeWay) {
+                    Thread.Sleep(500);
+                    File.Delete(playlistName);
+                }
             } else
                 Console.WriteLine("MPC-HC is not running.");
         }
--- src/CavernRemoteCGI/Extensions/Extensions.cs
+++ src/CavernRemoteCGI/Extensions/Extensions.cs
@@ -15,4 +15,15 @@ internal static class Extensions {
         return QueryFullProcessImageName(process.Handle, 0, fileNameBuilder, ref bufferLength) ?
             fileNameBuilder.ToString() : null;
     }
+
+    // https://stackoverflow.com/questions/108819/best-way-to-randomize-an-array-with-net
+    public static void Shuffle<T>(this Random rng, T[] array) {
+        int n = array.Length;
+        while (n > 1) {
+            int k = rng.Next(n--);
+            T temp = array[n];
+            array[n] = array[k];
+            array[k] = temp;
+        }
+    }
 }
